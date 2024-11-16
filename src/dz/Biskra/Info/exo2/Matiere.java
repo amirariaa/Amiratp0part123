654584091617ee12493645c74ab6fb5a50a8d7dd
user <user@DESKTOP-7E9FSKP>
2024-11-16 20:54:23 +0100

    init commit

--- src/dz/Biskra/Info/exo2/Matiere.java
+++ src/dz/Biskra/Info/exo2/Matiere.java
@@ -1,5 +1,24 @@
 package dz.Biskra.Info.exo2;
 
 public class Matiere {
+    private String intitule;
+    private double coefficient;
 
+    
+    public Matiere(String intitule, double coefficient) {
+        this.intitule = intitule;
+        this.coefficient = coefficient;
+    }
+
+    
+    public double getCoefficient() {
+        return coefficient;
+    }
+
+   
+    public String getIntitule() {
+        return intitule;
+    }
 }
+
+
